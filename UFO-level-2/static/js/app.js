4350cf9c692c72b5e9412adce986797f7468d725
bverb098 <102575708+bverb098@users.noreply.github.com>
2022-08-16 15:49:45 +1000

    changed input boxes to dropdown menus becuase I like them better

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -3,19 +3,76 @@ var sightings = data;
 var tbody = d3.select("tbody")
 
 var filterButton = d3.select("#filter-btn")
-
 var showallButton = d3.select("#showall-btn")
 
-filterButton.on("click",generateTable)
+var cityDropdown= d3.select("#city")
+var stateDropdown=d3.select("#state")
+var countryDropdown=d3.select("#country")
+var shapeDropdown=d3.select("#shape")
 
+filterButton.on("click",generateTable)
 showallButton.on("click",fullTable)
+
+function init() {
+	//create city dropdown menu
+	var cities = []
+	sightings.forEach(sighting => {
+		var city=sighting.city
+		if (!cities.includes(city)) {
+			cities.push(city)
+		}
+	});
+
+	cities.forEach(city => {
+		var cityOption = cityDropdown.append("option").text(city)
+	});
+
+	//create state dropdown menu
+	var states = []
+	sightings.forEach(sighting => {
+		var state=sighting.state
+		if (!states.includes(state)) {
+			states.push(state)
+		}
+		
+	});
+
+	states.forEach(state => {
+		var stateOption = stateDropdown.append("option").text(state)
+	});
+
+	//create country dropdown menu
+	var countries = []
+	sightings.forEach(sighting => {
+		var country=sighting.country
+		if (!countries.includes(country)) {
+			countries.push(country)
+		}
+	});
+
+	countries.forEach(country => {
+		var countryOption = countryDropdown.append("option").text(country)
+	});
+
+	//create shape dropdown menu
+	var shapes = []
+	sightings.forEach(sighting => {
+		var shape=sighting.shape
+		if (!shapes.includes(shape)) {
+			shapes.push(shape)
+		}
+	});
+
+	shapes.forEach(shape => {
+		var shapeOption = shapeDropdown.append("option").text(shape)
+	});
+		
+};
 
 function generateTable() {
 
 	tbody.html("")
 
-	d3.event.preventDefault();
-	
 	var inputDate = d3.select("#datetime").property("value")
 
 	var inputCity = d3.select("#city").property("value")
@@ -26,19 +83,32 @@ function generateTable() {
 
 	var inputShape = d3.select("#shape").property("value")
 
-	if (inputDate) {
-		var filteredSightings = sightings.filter(sighting => sighting.datetime === inputDate)
-	};
-	if (inputCity) {
-		var filteredSightings = sightings.filter(sighting => sighting.city === inputCity);
+	var filters = {}
+	if(inputDate) {
+		filters["datetime"] = inputDate
 	}
-	if (inputCountry) {
-	var filteredSightings = sightings.filter(sighting => sighting.country === inputCountry);
-	};
-	if (inputShape) {
-		var filteredSightings = sightings.filter(sighting => sighting.shape === inputShape)
-	};
-		
+	if(inputCity) {
+		filters["city"] = inputCity
+	}
+	if(inputState) {
+		filters["state"] = inputState
+	}
+	if(inputCountry) {
+		filters["country"] = inputCountry
+	}
+	if(inputShape) {
+		filters["shape"] = inputShape
+	}
+
+
+	console.log(filters)	
+	var filteredSightings=sightings
+	Object.entries(filters).forEach(([key,value]) => {
+		filteredSightings = filteredSightings.filter(sighting => {
+			return sighting[key] === value
+		})
+	});
+	
 	filteredSightings.forEach((sighting) => {
 		var row = tbody.append("tr");
 		Object.entries(sighting).forEach(([key,value]) => {
@@ -61,5 +131,7 @@ function fullTable () {
 			cell.text(value);
 		});
 	});
-}
+};
+
+init();
 	
